5260ffb89e712152ef4b7679402cab920324fc11
aggre <hiroyuki.aggre@gmail.com>
2020-04-28 14:01:09 +0900

    add test cases

--- sign/recover.test.ts
+++ sign/recover.test.ts
@@ -10,5 +10,26 @@ test(`Returns account's address from passed message and signature`, (t) => {
 	const account = web3.eth.accounts.create()
 	const signature = web3.eth.accounts.sign(message, account.privateKey)
 	const result = recover(signature.message, signature.signature)
-	t.is(result.toLowerCase(), account.address.toLowerCase())
+	t.is((result as string).toLowerCase(), account.address.toLowerCase())
+})
+
+test('Returns incorrect address when passed an incorrect message and a correct signature', (t) => {
+	const web3 = new Web3()
+	const account = web3.eth.accounts.create()
+	const signature = web3.eth.accounts.sign(message, account.privateKey)
+	const result = recover('incorrect message', signature.signature)
+	t.not((result as string).toLowerCase(), account.address.toLowerCase())
+})
+
+test('Returns error when passed a correct message and an incorrect signature', (t) => {
+	const web3 = new Web3()
+	const account = web3.eth.accounts.create()
+	const signature = web3.eth.accounts.sign(message, account.privateKey)
+	const result = recover(signature.message, 'incorrect signature')
+	t.is(result, undefined)
+})
+
+test('Returns error when passed an incorrect message and an incorrect signature', (t) => {
+	const result = recover('incorrect message', 'incorrect signature')
+	t.is(result, undefined)
 })
--- sign/recover.ts
+++ sign/recover.ts
@@ -1,5 +1,12 @@
 import Web3 from 'web3'
+import { tryCatch, always } from 'ramda'
 
-export const recover = (message: string, signature: string): string => {
-	return new Web3().eth.accounts.recover(message, signature)
+export const recover = (
+	message: string,
+	signature: string
+): string | undefined => {
+	return tryCatch(
+		(m, s) => new Web3().eth.accounts.recover(m, s),
+		always(undefined)
+	)(message, signature)
 }
