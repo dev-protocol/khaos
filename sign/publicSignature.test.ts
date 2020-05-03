7962eb3c0f680b6ddf6ade40122e63b33215468e
aggre <hiroyuki.aggre@gmail.com>
2020-05-04 00:24:42 +0900

    add test cases

--- sign/publicSignature.test.ts
+++ sign/publicSignature.test.ts
@@ -1,6 +1,10 @@
 import test from 'ava'
-import { sign } from 'jsonwebtoken'
+import { sign, verify } from 'jsonwebtoken'
 import { publicSignature } from './publicSignature'
+import { tryCatch, always } from 'ramda'
+
+const random = (): string =>
+	sign(Math.random().toString(), Math.random().toString())
 
 test('Returns signature from the passed message, address and sign method name', (t) => {
 	const message = 'A'
@@ -12,8 +16,6 @@ test('Returns signature from the passed message, address and sign method name', 
 })
 
 test('Returns the same result when the same arguments', (t) => {
-	const random = (): string =>
-		sign(Math.random().toString(), Math.random().toString())
 	const message = random()
 	const id = random()
 	const account = random()
@@ -27,8 +29,6 @@ test('Returns the same result when the same arguments', (t) => {
 })
 
 test('Returns the different result when the different arguments', (t) => {
-	const random = (): string =>
-		sign(Math.random().toString(), Math.random().toString())
 	const message = random()
 	const id = random()
 	const account = random()
@@ -39,4 +39,31 @@ test('Returns the different result when the different arguments', (t) => {
 	t.not(result1, sig)
 	t.not(result2, sig)
 	t.not(result3, sig)
+})
+
+test('A created signature can be verified by the account address', (t) => {
+	const message = random()
+	const id = random()
+	const account = random()
+	const pubSig = publicSignature({ message, id, account })
+	const verified = verify(pubSig, account)
+	const expected = `%${message}%-%${id}%`
+	t.is(verified, expected)
+})
+
+test('Should fail to verify when an account address is mismatch', (t) => {
+	const message = random()
+	const id = random()
+	const account = random()
+	const pubSig = publicSignature({ message, id, account })
+	const check = tryCatch(
+		(sig: string, account: string) => verify(sig, account),
+		always(undefined)
+	)
+	const verified1 = check(pubSig, `0${account}`)
+	const verified2 = check(pubSig, account.toLowerCase())
+	const verified3 = check(pubSig, account.toUpperCase())
+	t.is(verified1, undefined)
+	t.is(verified2, undefined)
+	t.is(verified3, undefined)
 })
