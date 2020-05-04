44a2fdf2b1a115ad2145bb01d94256955fd9bc2b
aggre <hiroyuki.aggre@gmail.com>
2020-05-04 14:37:59 +0900

    add verify API

---
+++ verify/index.test.ts
@@ -0,0 +1,55 @@
+/* eslint-disable functional/no-throw-statement */
+import test from 'ava'
+import * as db from '../sign/db'
+import * as jwt from 'jsonwebtoken'
+import { stub } from 'sinon'
+import { verify } from '.'
+
+test.serial(
+	'Returns true when the passed id is already saved on DB and verify succeeded',
+	async (t) => {
+		const account = 'S1zxiLd8jb4u@KKz7IDNlfz4&OPR@&j&EZkX'
+		const message = '6q7#kbquJA%PradxeMA9wtkNY$M5F%nd1P55'
+		const pubSigAsId = jwt.sign(message, account)
+		const stubbedReader = stub(db, 'reader').callsFake(() => async () =>
+			({
+				resource: { secret: '' },
+			} as any)
+		)
+		const result = await verify(pubSigAsId, account)
+		t.is(result, true)
+		stubbedReader.restore()
+	}
+)
+
+test.serial(
+	'Returns false when the passed id is already saved on DB but verify failed',
+	async (t) => {
+		const account = 'S1zxiLd8jb4u@KKz7IDNlfz4&OPR@&j&EZkX'
+		const message = '6q7#kbquJA%PradxeMA9wtkNY$M5F%nd1P55'
+		const pubSigAsId = jwt.sign(message, account)
+		const stubbedReader = stub(db, 'reader').callsFake(() => async () =>
+			({
+				resource: { secret: '' },
+			} as any)
+		)
+		const result = await verify(`0${pubSigAsId}`, account)
+		t.is(result, false)
+		stubbedReader.restore()
+	}
+)
+
+test.serial(
+	'Returns false when the passed id is not founded on DB',
+	async (t) => {
+		const account = 'S1zxiLd8jb4u@KKz7IDNlfz4&OPR@&j&EZkX'
+		const message = '6q7#kbquJA%PradxeMA9wtkNY$M5F%nd1P55'
+		const pubSigAsId = jwt.sign(message, account)
+		const stubbedReader = stub(db, 'reader').callsFake(() => async () => {
+			throw new Error()
+		})
+		const result = await verify(pubSigAsId, account)
+		t.is(result, false)
+		stubbedReader.restore()
+	}
+)
---
+++ verify/index.ts
@@ -0,0 +1,11 @@
+import { reader } from '../sign/db'
+import { CosmosClient } from '@azure/cosmos'
+import { verify as _verify } from 'jsonwebtoken'
+import { tryCatch, F } from 'ramda'
+
+export const verify = async (id: string, account: string): Promise<boolean> => {
+	const savedSecret = await reader(CosmosClient)(id).catch((err: Error) => err)
+	return savedSecret instanceof Error
+		? false
+		: tryCatch((i: string, a: string) => Boolean(_verify(i, a)), F)(id, account)
+}
