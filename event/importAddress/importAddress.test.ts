87d397e2beeb151a653e8a122dbd2c27bd35d390
aggre <hiroyuki.aggre@gmail.com>
2020-05-30 02:52:24 +0900

    fix broken test

--- event/importAddress/importAddress.test.ts
+++ event/importAddress/importAddress.test.ts
@@ -1,7 +1,7 @@
 import test from 'ava'
 import example from '../../functions/example/address'
 import { importAddress } from './importAddress'
-import { always, F } from 'ramda'
+import { always } from 'ramda'
 
 test('Returns a function that exists in functions/address', async (t) => {
 	const result = await importAddress('example')
@@ -11,6 +11,6 @@ test('Returns a function that exists in functions/address', async (t) => {
 
 test('Returns a function that always returns false when not exists in functions/address', async (t) => {
 	const result = await importAddress('*30%Cj*')
-	const expected = always(F)()
+	const expected = always(undefined)
 	t.is(result.toString(), expected.toString())
 })
