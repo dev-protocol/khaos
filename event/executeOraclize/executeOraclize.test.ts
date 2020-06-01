3c8767325ae485e5bed12a0911312882ae49b898
Akira-Taniguchi <dededededaiou2003@yahoo.co.jp>
2020-06-01 16:16:28 +0900

    add execute oracle test

---
+++ event/executeOraclize/executeOraclize.test.ts
@@ -0,0 +1,13 @@
+// import test from 'ava'
+// import { ItemResponse } from '@azure/cosmos'
+// import { executeOraclize } from './executeOraclize'
+// import { oracleArgInfo } from './../getSecret/getSecret'
+// import { Secret } from './../../common/db/secret'
+
+// test('write; insert new data to `Authentication.LastBlock`', async (t) => {
+// const arg: oracleArgInfo = {
+// 	secret: new ItemResponse<Secret>(),
+// 	json: {},
+// }
+// await executeOraclize(arg)
+//})
