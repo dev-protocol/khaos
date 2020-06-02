2d4c9e61e98e8bdb5e3d9d972daee34e6c3e9cee
Akira-Taniguchi <dededededaiou2003@yahoo.co.jp>
2020-06-02 12:54:56 +0900

    add getLastBlock test

---
+++ event/getLastBlock/getLastBlock.test.ts
@@ -0,0 +1,31 @@
+import test from 'ava'
+import { stub } from 'sinon'
+import * as lastBlock from '../db/db'
+import { getLastBlock } from './getLastBlock'
+
+test.serial(
+	'If the record exists, return the lastBlock that has been recorded.',
+	async (t) => {
+		const stubbedReader = stub(lastBlock, 'reader').callsFake(() => async () =>
+			({
+				statusCode: 200,
+				resource: { lastBlock: 100 },
+			} as any)
+		)
+		const result = await getLastBlock('dummy-khaos-id')
+		t.is(result, 100)
+		stubbedReader.restore()
+	}
+)
+
+test.serial('If the record does not exist, return 0.', async (t) => {
+	const stubbedReader = stub(lastBlock, 'reader').callsFake(() => async () =>
+		({
+			statusCode: 200,
+			resource: undefined,
+		} as any)
+	)
+	const result = await getLastBlock('dummy-khaos-id')
+	t.is(result, 0)
+	stubbedReader.restore()
+})
