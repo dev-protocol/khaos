348eea188d453e4f2508cbcc0dd21c07d0ee5c55
aggre <hiroyuki.aggre@gmail.com>
2020-08-22 00:21:01 +0900

    add function: recoverPublicSignature

---
+++ sign/publicSignature/recoverPublicSignature.ts
@@ -0,0 +1,22 @@
+import { verify } from 'jsonwebtoken'
+import { tryCatch, always } from 'ramda'
+import { when } from '../../common/util/when'
+import { PublicSignatureOptions } from './publicSignature'
+
+export const recoverPublicSignature = (
+	publicSignature: string,
+	address: string
+): PublicSignatureOptions | undefined => {
+	const recover = tryCatch(
+		(i: string, a: string) => verify(i, a),
+		always(undefined)
+	)(publicSignature, address)
+	const parsed = when(recover, (rec) =>
+		typeof rec === 'string'
+			? rec.replace(/(^%|%$)/g, '').split('%-%')
+			: undefined
+	)
+	return parsed
+		? (([message, id]) => ({ message, id, address }))(parsed)
+		: parsed
+}
