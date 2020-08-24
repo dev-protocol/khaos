a2c1104310759b101c553cee77f40eb49c116cb0
Akira-Taniguchi <dededededaiou2003@yahoo.co.jp>
2020-08-24 16:14:50 +0900

    update ropsten address information

--- functions/github-market/addresses.ts
+++ functions/github-market/addresses.ts
@@ -1,5 +1,5 @@
 import { Addresses } from '../addresses'
 
-const fn: Addresses = async (net) => (net === 'mainnet' ? '0x1' : '0x2')
+const fn: Addresses = async (net) => (net === 'mainnet' ? '0x1' : '0x7222ec13791b8A52376D21Ab9054D12017Ad5e7A')
 
 export default fn
