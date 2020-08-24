c4adcd1fbe6c6eecea8107eaa92281e9baf2fc84
Akira-Taniguchi <dededededaiou2003@yahoo.co.jp>
2020-08-24 16:35:17 +0900

    update contract address

--- functions/github-market/addresses.ts
+++ functions/github-market/addresses.ts
@@ -1,6 +1,6 @@
 import { Addresses } from '../addresses'
 
 const fn: Addresses = async (net) =>
-	net === 'mainnet' ? '0x1' : '0x7222ec13791b8A52376D21Ab9054D12017Ad5e7A'
+	net === 'mainnet' ? '0x1' : '0xea76F3d6340f6aC253305fD7ddF2831A3BE4D347'
 
 export default fn
