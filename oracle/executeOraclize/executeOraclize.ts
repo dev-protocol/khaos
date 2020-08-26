deb2dda6056e20e4396419853cd61fb547ab5fab
aggre <hiroyuki.aggre@gmail.com>
2020-08-26 23:19:41 +0900

    fix broken import path

--- oracle/executeOraclize/executeOraclize.ts
+++ oracle/executeOraclize/executeOraclize.ts
@@ -1,8 +1,8 @@
 import { oracleArgInfo } from './../getSecret/getSecret'
 import { importOraclize } from '../importOraclize/importOraclize'
-import { when } from '../../common/util/when'
 import { recoverPublicSignature } from '../../sign/publicSignature/recoverPublicSignature'
 import { KhaosCallbackArg } from '../../functions/oraclize'
+import { whenDefined } from '../../common/util/whenDefined'
 
 export type sendInfo = {
 	readonly khaosId: string
@@ -13,10 +13,12 @@ export const executeOraclize = (id: string) => async (
 	info: oracleArgInfo
 ): Promise<sendInfo> => {
 	const oraclize = await importOraclize(id)
-	const recoverd = when(info.secret.resource, ({ id, address }) =>
+	const recoverd = whenDefined(info.secret.resource, ({ id, address }) =>
 		recoverPublicSignature(id, address)
 	)
-	const callBack = await when(recoverd, (r) => oraclize(r, info.eventData))
+	const callBack = await whenDefined(recoverd, (r) =>
+		oraclize(r, info.eventData)
+	)
 	return {
 		khaosId: id,
 		result: typeof callBack === 'undefined' ? undefined : callBack,
