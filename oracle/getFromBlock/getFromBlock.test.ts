ecb2aea823950ef9f8bebaa58e75590d07e4e37e
Akira-Taniguchi <dededededaiou2003@yahoo.co.jp>
2020-09-03 17:06:34 +0900

    update test case

--- oracle/getFromBlock/getFromBlock.test.ts
+++ oracle/getFromBlock/getFromBlock.test.ts
@@ -2,27 +2,18 @@
 import test from 'ava'
 import { getFromBlock } from './getFromBlock'
 
-test.serial(
-	'If KHAOS_BLOCK_RANGE is not set, it returns the number minus 80..',
-	async (t) => {
-		const result = getFromBlock(100)
-		t.is(result, 20)
-	}
-)
+test('If KHAOS_BLOCK_RANGE is not set, it returns the number minus 80..', async (t) => {
+	const result = getFromBlock(100)
+	t.is(result, 20)
+})
 
-test.serial(
-	'If undefined is passed, undefined will be returned.',
-	async (t) => {
-		const result = getFromBlock(undefined)
-		t.is(result, undefined)
-	}
-)
+test('If undefined is passed, undefined will be returned.', async (t) => {
+	const result = getFromBlock(undefined)
+	t.is(result, undefined)
+})
 
-test.serial(
-	'Return the specified value minus the value you set for KHAOS_BLOCK_RANGE.',
-	async (t) => {
-		process.env.KHAOS_BLOCK_RANGE = '50'
-		const result = getFromBlock(100)
-		t.is(result, 50)
-	}
-)
+test('Return the specified value minus the value you set for KHAOS_BLOCK_RANGE.', async (t) => {
+	process.env.KHAOS_BLOCK_RANGE = '50'
+	const result = getFromBlock(100)
+	t.is(result, 50)
+})
