4722f51b5d81a7794445a3f588a30b4ccc47d740
aggre <hiroyuki.aggre@gmail.com>
2021-01-15 22:21:37 +0900

    supports `event` and `pack`

--- oracle/sendContractMethod/sendContractMethod.test.ts
+++ oracle/sendContractMethod/sendContractMethod.test.ts
@@ -12,9 +12,9 @@ const dummyConstract = {
 }
 
 test('event information is coming back.', async (t) => {
-	const result = await sendContractMethod(dummyConstract as any)({
-		result: 'dummy-result',
-	} as any)
+	const result = await sendContractMethod(
+		dummyConstract as any
+	)('khaosCallback', ['test', 0])
 	t.deepEqual(result, {
 		hash: '0xdummy',
 	} as any)
