838ae318a82fdae578a1686e538931667d22f369
aggre <hiroyuki.aggre@gmail.com>
2021-02-06 13:27:45 +0900

    improve event args getter

--- oracle/getData/getData.ts
+++ oracle/getData/getData.ts
@@ -1,10 +1,15 @@
 import { ethers } from 'ethers'
+import { mergeAll } from 'ramda'
 import { MarketQueryData } from '../../common/structs'
 
 export const getData = function (event: ethers.Event): MarketQueryData {
+	const src = event.args || []
+	const keys = Object.keys(src)
+	const data = keys.map(key => ({[key]: src[key]}))
+	const allData = mergeAll(data)
 	return {
-		publicSignature: event.args?.['publicSignature'],
-		allData: event.args,
+		publicSignature: allData.publicSignature,
+		allData,
 		transactionhash: event.transactionHash,
 	} as MarketQueryData
 }
