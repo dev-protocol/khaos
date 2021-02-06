de2900e34013bfeb7ff6a1bc669d2299ef3f6bf9
aggre <hiroyuki.aggre@gmail.com>
2021-02-06 13:28:19 +0900

    apply formatter

--- oracle/getData/getData.ts
+++ oracle/getData/getData.ts
@@ -5,7 +5,7 @@ import { MarketQueryData } from '../../common/structs'
 export const getData = function (event: ethers.Event): MarketQueryData {
 	const src = event.args || []
 	const keys = Object.keys(src)
-	const data = keys.map(key => ({[key]: src[key]}))
+	const data = keys.map((key) => ({ [key]: src[key] }))
 	const allData = mergeAll(data)
 	return {
 		publicSignature: allData.publicSignature,
