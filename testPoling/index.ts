d23e6bb6339af384fb368b12660d131b29b56ce5
Akira-Taniguchi <dededededaiou2003@yahoo.co.jp>
2021-11-10 10:11:08 +0900

    add test

---
+++ testPoling/index.ts
@@ -0,0 +1,24 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+/* eslint-disable functional/immutable-data */
+/* eslint-disable functional/no-expression-statement */
+import { AzureFunction, Context, HttpRequest } from '@azure/functions'
+import {idProcessTest} from './idProcessTest'
+
+const httpTrigger: AzureFunction = async function (
+	context: Context,
+	req: HttpRequest
+): Promise<void> {
+	const sBlock = req.body.startBlock
+	const eBlock = req.body.endBlock
+	await idProcessTest(context, 'arbitrum-one')('youtube-market', sBlock, eBlock)
+
+	context.res = {
+		status: 0,
+		body: '',
+		headers: {
+			'Cache-Control': 'no-store',
+		},
+	}
+}
+
+export default httpTrigger
